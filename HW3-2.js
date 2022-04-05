b7a42f5da6dd9a5fadc672730afcdd93a9233dc8
Oleg <kolesnikovovov@gmail.com>
2022-04-05 19:15:51 +0500

    ДЗ

---
+++ HW3-2.js
@@ -0,0 +1,21 @@
+let i = getRandomInt(1,10);// рандомны размер массива
+let basket = []; //создание массива
+
+for (a = 0; a != i; a++)//заполнение массива
+{
+basket.push(getRandomInt(1,1000));
+}
+
+console.log("Basket Price = ", countBasketPrice(basket));
+
+function getRandomInt(min, max) {//рандомные числа в диапозоне
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+function countBasketPrice(arr){//суммарная цена
+    let Price = 0;
+    for (I = 0; I != arr.length; I++){
+        Price += arr[I];
+    }
+    return Price;
+}
---
+++ HW3-5.js
@@ -0,0 +1,6 @@
+let str = "";
+for (i = 0; i <= 20; i++){
+    str +="x";
+    console.log(str);
+    console.log();
+}
